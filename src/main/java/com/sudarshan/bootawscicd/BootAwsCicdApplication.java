2f2fe1ea0d2479a03ddbc242b66354e20d329bb6
SudarshanKumar <sudarshan4309@gmail.com>
2021-08-21 23:14:49 +0530

    boot application for aws cicd on elasticbeanstalk

---
+++ src/main/java/com/sudarshan/bootawscicd/BootAwsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.sudarshan.bootawscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAwsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAwsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudarshan/bootawscicd/BootAwsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudarshan.bootawscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAwsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
